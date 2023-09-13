b7493910a37ef8c0108171385ede2493b3b140ad
dragomirNonov <nonov.dragomir@gmail.com>
2023-09-13 00:53:37 -0500

    changed components directory

--- cosc4353-project/src/App.jsx
+++ cosc4353-project/src/App.jsx
@@ -1,5 +1,5 @@
-import LoginForm from "../components/login";
-import RegisterForm from "../components/register";
+import LoginForm from "./components/login";
+import RegisterForm from "./components/register";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 const App = () => {
   return (
