f79fa919d987e8efbc5c0ba4d056aa1493fdd296
Vian <89276801+vpluu@users.noreply.github.com>
2023-09-16 18:11:07 -0500

    Added profile and account settings pages, a corresponding settings.css file, and added pages to Routes

--- cosc4353-project/src/App.jsx
+++ cosc4353-project/src/App.jsx
@@ -2,6 +2,8 @@ import LoginForm from "./components/login";
 import RegisterForm from "./components/register";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navigation from "./components/navigation";
+import ProfileSettings from "./components/profilesettings";
+import AccountSettings from "./components/accountsettings";
 const App = () => {
   return (
     <div className="body">
@@ -10,6 +12,8 @@ const App = () => {
           <Route path="/" element={<LoginForm />} />
           <Route path="/register" element={<RegisterForm />} />
           <Route path="/home" element={<Navigation />} />
+          <Route path="/profilesettings" element={<ProfileSettings />} />
+          <Route path="/accountsettings" element={<AccountSettings />} />
         </Routes>
       </Router>
     </div>
---
+++ cosc4353-project/src/components/accountsettings.jsx
@@ -0,0 +1,96 @@
+import { BrowserRouter as Router, Link } from "react-router-dom";
+import { useState } from "react";
+import ProfileSettings from "./profilesettings";
+import "../settings.css";
+
+const AccountSettings = (props) => {
+    const[email, setEmail] = useState("");
+    const[password, setPassword] = useState("");
+
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+        //console.log(event.target.value);
+    };
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+        //console.log(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        //console.log(event.target.value);
+        //console.log(event.target.value);
+    }
+
+    // Go To Profile Settings Function
+
+    const[settingsPage, setSettingsPage] = useState("");
+
+    const setPage = (settingsPage) => {
+        setSettingsPage = (settingsPage);
+    }
+
+    return (
+        <div className="gradient-background">
+            <div className="main-container">
+                <div id="settings-tabs">
+                    <ul className="no-bullets">
+                        <li id="settings">Settings</li>
+                        <li>
+                            <button 
+                            className = "settings-button"
+                            onClick = {() => setPage(<ProfileSettings />)}>  Profile</button>
+                        </li>
+                        <li>
+                            <button 
+                            className = "settings-button"
+                            onClick = {() => setSettingsPage(<AccountSettings />)}>  Account Info</button>
+                        </li>
+                    </ul>
+                </div>
+
+                {settingsPage === "profile" && <ProfileSettings />}
+
+                <div className="settings-container">
+                    <h1>Edit your account</h1>
+
+                    <form onSubmit = {handleSubmit}>
+                        <div>
+                            <label htmlFor="email">Email</label>
+                            <input 
+                                className="textarea"
+                                type="text" 
+                                name="email" 
+                                id="email" 
+                                value={email}
+                                onChange={handleEmailChange}
+                                pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}"
+                            />
+                        </div>
+                        <div>
+                            <div id="password-container">
+                                <label htmlFor="password">Password</label>
+                                <input 
+                                    className="textarea"
+                                    type="text" 
+                                    name="password" 
+                                    id="password"
+                                    value={password}
+                                    onChange={handlePasswordChange}
+                                />
+                            </div>
+                        </div>
+                        <button
+                            className="save-button" 
+                            type="submit">
+                            Save
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default AccountSettings;
