a7a3bbfeca5583880da96d63e01b7e7fffd0e25d
Vian <89276801+vpluu@users.noreply.github.com>
2023-10-09 14:21:28 -0500

    Added authentication to profilesettings.js and checks if user input is not empty

--- backend/routes/profileSettings.js
+++ backend/routes/profileSettings.js
@@ -1,32 +1,32 @@
 const express = require("express");
 const router = express.Router();
 const users = require("../data/usersData");
-
-/* 
-    Replace with user authentication (token/session)
-
-const { authenticateUser, updateUserProfile } = require("../middleware/auth");
-    */
-
+const jwt = require("jsonwebtoken");
+const { authUser } = require("../services/basicAuth");
 
 // Update user profile info
-router.put("/api/users/updateProfile", authenticateUser, (request, response) => {
-    try {
-        const firstName = request.body.firstName;
-        const lastName = request.body.lastName;
-        const address1 = request.body.address1;
-        const address2 = request.body.address2;
-        const city = request.body.city;
-        const state = request.body.state;
-        const zipCode = request.body.zipeCode;
+router.put("/api/users/updateProfile", (request, response) => {
 
-        // Fetch the currently logged-in user
-        const user = request.user;
+    // get token from headers
+    const token = request.headers["token"];
+    const decoded = jwt.verify(token, "secretkey");
+    const userID = decoded.userId;
+    try {
+        const user = users.find((user) => user.id === userID);
     
         if (!user) {
           return response.status(404).json({ message: "User not found" });
         }
         else {
+
+            // Get information from input
+            const { firstName, lastName, address1, address2, city, state, zipCode } = request.body;
+
+            // Check for required fields
+            if (!firstName || !lastName || !address1 || !city || !state || !zipCode) {
+            return response.status(400).json({ message: "Please fill out all required fields" });
+            }
+
             // Update user's profile information
             user.firstName = firstName;
             user.lastName = lastName;
@@ -35,16 +35,11 @@ router.put("/api/users/updateProfile", authenticateUser, (request, response) => 
             user.city = city;
             user.state = state;
             user.zipCode = zipCode;
+            user.isProfileComplate = true;
 
             // Save the updated user profile
-            user.save((error) => {
-                if (error) {
-                    console.error("Error updating profile: ", error);
-                    response.status(500).json({ message: "Internal server error" });
-                } else {
-                    response.status(200).json({ message: "Profile updated successfully" });
-                }
-            });
+            response.status(200).json({ message: "Profile updated successfully" });
+
         }
     } catch (error) {
         console.error("Error updating profile: ", error);
