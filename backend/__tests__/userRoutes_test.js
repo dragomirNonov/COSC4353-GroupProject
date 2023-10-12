f8a8aad3f3e885864d6a9675e7f6dbc57301ef49
dragomirNonov <nonov.dragomir@gmail.com>
2023-10-11 20:09:36 -0500

    added tests for // LOGIN //REGISTER //Get User by ID

--- backend/__tests__/userRoutes_test.js
+++ backend/__tests__/userRoutes_test.js
@@ -1,5 +1,7 @@
 const request = require("supertest");
 const app = require("../index"); // Adjust the relative path as needed
+const jwt = require("jsonwebtoken");
+const users = require("../data/usersData");
 
 describe("POST /api/login", () => {
   it("returns 401 for an unknown user", async () => {
@@ -21,5 +23,67 @@ describe("POST /api/login", () => {
     expect(response.body).toHaveProperty("isProfileComplate");
   });
 
-  // Add more test cases for edge cases and error conditions
+  it("returns 404 for a valid token with an unknown user ID", async () => {
+    // Create a valid token with a non-existent user ID
+    const token = jwt.sign({ userId: 777 }, "secretkey");
+
+    const response = await request(app)
+      .get("/api/users/id")
+      .set("token", token);
+
+    expect(response.status).toBe(404);
+    expect(response.body).toEqual({ message: "User not found" });
+  });
+
+  it("returns 200 for a valid token with an unknown user ID", async () => {
+    // Create a valid token with a non-existent user ID
+    const token = jwt.sign({ userId: 300 }, "secretkey");
+
+    const response = await request(app)
+      .get("/api/users/id")
+      .set("token", token);
+
+    expect(response.status).toBe(200);
+  });
+
+  it("creates a new user and returns a 201 status for valid input", async () => {
+    const newUser = {
+      username: "newUser",
+      password: "password123",
+      email: "newuser@example.com",
+    };
+
+    const response = await request(app).post("/api/users").send(newUser);
+
+    expect(response.status).toBe(201);
+    expect(response.body).toEqual({ message: "User added successfully" });
+
+    // You can add additional assertions to check the user creation logic here.
+  });
+
+  it("returns a 400 status for an existing username", async () => {
+    // Add a user with an existing username to the 'users' array for testing
+    const existingUser = {
+      username: "existingUser",
+      password: "existingPassword",
+      email: "existinguser@example.com",
+    };
+    users.push(existingUser);
+
+    const newUserWithDuplicateUsername = {
+      username: "existingUser",
+      password: "newPassword",
+      email: "newuser@example.com",
+    };
+
+    const response = await request(app)
+      .post("/api/users")
+      .send(newUserWithDuplicateUsername);
+
+    expect(response.status).toBe(400);
+    expect(response.body).toEqual({ message: "User already exists" });
+
+    // Remove the test user added to 'users' array to keep the data clean
+    users.pop();
+  });
 });
