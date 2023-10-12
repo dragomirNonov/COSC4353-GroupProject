beb0bc1f013fc435a80ae5a4525a7da20c5b64dc
dragomirNonov <nonov.dragomir@gmail.com>
2023-10-11 20:17:36 -0500

    added pricingModule.js and the getSuggestedPrice function that will later be used for calculating suggested price.

---
+++ backend/services/pricingModule.js
@@ -0,0 +1,5 @@
+//This will contain a function that calculates
+//the suggested price once the "Get Quote"
+//button on the Quote form is clicked.
+
+module.exports.getSuggestedPrice = (args) => {};
